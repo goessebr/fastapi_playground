b564633822f95a18e89f46fdc53cf5e3434e2da1
Bram Goessens <bram.goessens@vlaanderen.be>
2026-02-25 14:37:37 +0100

    security tests

--- app/security/auth.py
+++ app/security/auth.py
@@ -1,11 +1,20 @@
 # login, jwt, get_current_user
+from typing import Annotated
 
-async def get_current_user() -> dict:
-    if True:  # TODO: implement real authentication
+from fastapi import Header
+
+
+async def get_current_user(x_user_role: Annotated[str | None, Header()] = None) -> dict:
+    scopes = []
+    if x_user_role == "aangemeld":
+        scopes = ["me"]
+    if x_user_role == "admin":
+        scopes = ["me", "personen:read", "personen:read-basic", "personen:write", "organisaties:read", "organisaties:write"]
+    elif x_user_role == "basic":
+        scopes = ["me", "personen:read-basic"]
+    if x_user_role in ("admin", "basic", "aangemeld"):
         return {
             "username": "demo_user",
-            "scopes": ["me", "personen:read", "personen:read-basic", "personen:write", "organisaties:read", "organisaties:write"],
-            # "scopes": ["me", "personen:read-basic"], # simulate a user with basic permissions
-            # "scopes": [],  # simulate a user with no permissions
+            "scopes": scopes
         }
     return {}  # Return an empty dict for unauthenticated users
