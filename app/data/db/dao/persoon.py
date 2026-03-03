a363b2553a1e35d2960931de69a35d75aeb575fa
Bram Goessens <bram.goessens@vlaanderen.be>
2026-03-03 19:35:00 +0100

    verwijder attribute_names

--- app/data/db/dao/persoon.py
+++ app/data/db/dao/persoon.py
@@ -32,4 +32,4 @@ class PersoonDAO(BaseDAO):
         return result.scalars().first()
 
     async def save(self, persoon: Persoon) -> Persoon:
-        return await self.common.save(orm_object=persoon, attribute_names=["organisaties"])
+        return await self.common.save(orm_object=persoon)
