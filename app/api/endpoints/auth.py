6ef5b09d3f7853c68b04a83f1c397a3cdec78c5a
Bram Goessens <bram.goessens@vlaanderen.be>
2026-03-04 08:33:31 +0100

    typeddict ipv dict voor current user

--- app/api/endpoints/auth.py
+++ app/api/endpoints/auth.py
@@ -3,7 +3,8 @@ from typing import Annotated
 from fastapi import APIRouter
 from fastapi import Depends
 from app.api.dependencies import get_current_user
+from app.security.auth import CurrentUser
 
 router = APIRouter()
 
-CurrentUserDependency = Annotated[dict, Depends(get_current_user)]
+CurrentUserDependency = Annotated[CurrentUser, Depends(get_current_user)]
